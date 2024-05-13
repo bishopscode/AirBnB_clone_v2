4faf7ca99d3405879db9651884c78fe809b32239
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-05-13 17:50:19 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,16 @@
--- Prepares a MySQL server for the project.
+-- Task: Prepare MySQL server for the project
+
+-- Create the database hbnb_dev_db if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user hbnb_dev if it doesn't already exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+
+-- Grant all privileges on hbnb_dev_db to the user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the user hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
