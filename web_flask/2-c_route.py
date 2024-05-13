da1d03c132f19e580147c16292cc0fb179f8c7af
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-05-13 20:25:06 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
 """
+Script that starts a Flask web application:
+    listening on 0.0.0.0, port 5000
+    With three Routes
+    """
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+@app.route('/', strict_slashes=False)
+def hello():
+    """ First Route that display Hello HBNB"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'."""
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ Second Route that display HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def hello_text(text):
+    """ Third Route that display C and text"""
+    return "C {}".format(text.replace("_", " "))
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
