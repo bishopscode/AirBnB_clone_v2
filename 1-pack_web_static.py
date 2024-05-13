ec4b43b64b77287998d2e1fc728d3f4b916e8a48
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-05-13 17:13:58 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
-Module docs
-"""
-from fabric.api import task, local
+'''Fabric script to generate .tgz archive'''
+
+from fabric.api import local
 from datetime import datetime
 
+from fabric.decorators import runs_once
 
-@task
+
+@runs_once
 def do_pack():
-    """
-    Function Docs
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
+    '''generates .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    final = local("tar -cvzf {} web_static"
+                   .format(path))
+
+    if final.failed:
+        return None
+    return path
