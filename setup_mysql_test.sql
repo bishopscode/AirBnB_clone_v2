2f6982d7a2b53a45209d525ddf9b2d196e074d08
bishopscode <64908269+bishopscode@users.noreply.github.com>
2024-05-13 17:51:29 +0100

    Update setup_mysql_test.sql
    to test DB

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,24 @@
--- Prepares a MySQL Test server for the project.
+-- Check if the database hbnb_test_db exists
+SELECT SCHEMA_NAME
+FROM INFORMATION_SCHEMA.SCHEMATA
+WHERE SCHEMA_NAME = 'hbnb_test_db';
+
+-- If the database doesn't exist, create it
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Check if the user hbnb_test exists
+SELECT user
+FROM mysql.user
+WHERE user = 'hbnb_test';
+
+-- If the user doesn't exist, create it and grant privileges
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+
+-- Grant privileges to the user hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege to the user hbnb_test on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
